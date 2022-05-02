677cdfe0235dc0f6487e85d5b53d2d6fc700ff91
Kapperchino <kamuran12580@gmail.com>
2022-05-02 19:32:29 -0400

    fixed docker issue

--- app/bin/start-all.sh
+++ app/bin/start-all.sh
@@ -18,7 +18,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 USAGE="start-all.sh <example> <subcommand>"
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ]; then
   echo "$USAGE"
   exit 1
 fi
@@ -35,6 +35,9 @@ shift
 peers="$1"
 shift
 
+group="$1"
+shift
+
 
 # Find a tmpdir, defaulting to what the environment tells us
 tmp="${TMPDIR:-/tmp}"
@@ -42,8 +45,8 @@ tmp="${TMPDIR:-/tmp}"
 echo "Starting 3 Ratis servers with '${example}' with directories in '${tmp}' as local storage"
 
 # The ID needs to be kept in sync with QUORUM_OPTS
-$DIR/server.sh "$example" "$subcommand" --id n0 --storage "${tmp}/n0" --peers $peers &
-$DIR/server.sh "$example" "$subcommand" --id n1 --storage "${tmp}/n1" --peers $peers &
-$DIR/server.sh "$example" "$subcommand" --id n2 --storage "${tmp}/n2" --peers $peers &
+$DIR/server.sh "$example" "$subcommand" --id n0 --storage "${tmp}/n0" --peers $peers --raftGroup $group&
+$DIR/server.sh "$example" "$subcommand" --id n1 --storage "${tmp}/n1" --peers $peers --raftGroup $group&
+$DIR/server.sh "$example" "$subcommand" --id n2 --storage "${tmp}/n2" --peers $peers --raftGroup $group&
 
 echo "Waiting for the servers"
